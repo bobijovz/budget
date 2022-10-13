2fe0f47cd105de997052bdcf1397f7174664c6f0
Jovi Granados <j.granados@damstratechnology.com>
2022-10-14 01:06:27 +0800

    updates

--- data/src/main/java/com/bobijovz/data/entity/ActivityEntity.kt
+++ data/src/main/java/com/bobijovz/data/entity/ActivityEntity.kt
@@ -6,3 +6,12 @@ data class ActivityEntity(
     val description: String,
     val price: Double
 )
+
+/*
+@Entity
+data class ActivityEntity(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "category_id") val categoryId: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "price") val price: Double
+)*/
